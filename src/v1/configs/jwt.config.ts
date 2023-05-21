9da4c24f89c17eb893253cdc9cde73bba081d179
faizul-mustafiz <faizul.mustafiz@gmail.com>
2023-05-22 01:39:24 +0600

    Update environment imports for unnecessary imports and also update env.example file and update docker file

--- src/v1/configs/jwt.config.ts
+++ src/v1/configs/jwt.config.ts
@@ -1,14 +1,10 @@
 import { environment } from '../environments/index';
 
 const {
-  ACCESS_TOKEN_SECRET,
   ACCESS_TOKEN_EXPIRY_TIME,
-  REFRESH_TOKEN_SECRET,
   REFRESH_TOKEN_EXPIRY_TIME,
   VERIFY_TOKEN_SECRET,
   VERIFY_TOKEN_EXPIRY_TIME,
-  RESET_PASSWORD_TOKEN_SECRET,
-  RESET_PASSWORD_TOKEN_EXPIRY_TIME,
   CHANGE_PASSWORD_TOKEN_SECRET,
   CHANGE_PASSWORD_TOKEN_EXPIRY_TIME,
   PUBLIC_KEY,
@@ -16,23 +12,16 @@ const {
 } = environment;
 
 const accessTokenConfig = {
-  secret: ACCESS_TOKEN_SECRET,
   expiryTime: ACCESS_TOKEN_EXPIRY_TIME,
 };
 
 const refreshTokenConfig = {
-  secret: REFRESH_TOKEN_SECRET,
   expiryTime: REFRESH_TOKEN_EXPIRY_TIME,
 };
 
 const verifyTokenConfig = {
   secret: VERIFY_TOKEN_SECRET,
   expiryTime: VERIFY_TOKEN_EXPIRY_TIME,
-};
-
-const resetPasswordTokenConfig = {
-  secret: RESET_PASSWORD_TOKEN_SECRET,
-  expiryTime: RESET_PASSWORD_TOKEN_EXPIRY_TIME,
 };
 
 const changePasswordTokenConfig = {
@@ -44,7 +33,6 @@ export const JWTConfig = {
   accessTokenConfig,
   refreshTokenConfig,
   verifyTokenConfig,
-  resetPasswordTokenConfig,
   changePasswordTokenConfig,
   publicKey: PUBLIC_KEY,
   privateKey: PRIVATE_KEY,
--- src/v1/environments/environment.production.ts
+++ src/v1/environments/environment.production.ts
@@ -7,28 +7,15 @@ export const environment = {
   API_HOST: process.env.API_HOST as string,
   API_PORT: process.env.API_PORT as string,
   BASE_API_ROUTE: process.env.BASE_API_ROUTE as string,
-
   // redis environments variables
   REDIS_URL: process.env.REDIS_URL as string,
-  REDIS_HOST: process.env.REDIS_HOST as string,
-  REDIS_PORT: process.env.REDIS_PORT as string,
-  REDIS_USERNAME: process.env.REDIS_USERNAME as string,
-  REDIS_PASSWORD: process.env.REDIS_PASSWORD as string,
   // mongo environment variables
   MONGO_URL: process.env.MONGO_URL as string,
-  MONGO_USERNAME: process.env.MONGO_USERNAME as string,
-  MONGO_PASSWORD: process.env.MONGO_PASSWORD as string,
   // JWT environment variables
-  ACCESS_TOKEN_SECRET: process.env.ACCESS_TOKEN_SECRET as string,
   ACCESS_TOKEN_EXPIRY_TIME: process.env.ACCESS_TOKEN_EXPIRY_TIME as string,
-  REFRESH_TOKEN_SECRET: process.env.REFRESH_TOKEN_SECRET as string,
   REFRESH_TOKEN_EXPIRY_TIME: process.env.REFRESH_TOKEN_EXPIRY_TIME as string,
   VERIFY_TOKEN_SECRET: process.env.VERIFY_TOKEN_SECRET as string,
   VERIFY_TOKEN_EXPIRY_TIME: process.env.VERIFY_TOKEN_EXPIRY_TIME as string,
-  RESET_PASSWORD_TOKEN_SECRET: process.env
-    .RESET_PASSWORD_TOKEN_SECRET as string,
-  RESET_PASSWORD_TOKEN_EXPIRY_TIME: process.env
-    .RESET_PASSWORD_TOKEN_EXPIRY_TIME as string,
   CHANGE_PASSWORD_TOKEN_SECRET: process.env
     .CHANGE_PASSWORD_TOKEN_SECRET as string,
   CHANGE_PASSWORD_TOKEN_EXPIRY_TIME: process.env
